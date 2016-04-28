b638b4446535c5251e030efb10fa35abb2e1bec0
furuihao <94402146@qq.com>
2016-04-28 17:53:07 +0800

    resultMap,动态Sql，sql片段

--- studies.mybatis/src/main/java/com/hao/studies/mybatis/mapper/PersonMapper.java
+++ studies.mybatis/src/main/java/com/hao/studies/mybatis/mapper/PersonMapper.java
@@ -1,15 +1,22 @@
 package com.hao.studies.mybatis.mapper;
 
+import java.util.List;
+
 import com.hao.studies.mybatis.models.Person;
+import com.hao.studies.mybatis.models.PersonVo;
 
 public interface PersonMapper {
 	public Person findById(int id) throws Exception;
 
-//	public List<Person> findByName(String name) throws Exception;
-//
-//	public void save(Person p) throws Exception;
-//
-//	public void delete(Person p) throws Exception;
-//
-//	public void update(Person p) throws Exception;
+	public Person findByIdResultMap(int id) throws Exception;
+
+	public List<Person> findPersonList(PersonVo personVo) throws Exception;
+
+	// public List<Person> findByName(String name) throws Exception;
+	//
+	// public void save(Person p) throws Exception;
+	//
+	// public void delete(Person p) throws Exception;
+	//
+	// public void update(Person p) throws Exception;
 }
--- studies.mybatis/src/test/java/com/hoa/studies/person/PersonMapperTest.java
+++ studies.mybatis/src/test/java/com/hoa/studies/person/PersonMapperTest.java
@@ -1,11 +1,16 @@
 package com.hoa.studies.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import com.hao.studies.mybatis.dao.SessionFactory;
 import com.hao.studies.mybatis.mapper.PersonMapper;
 import com.hao.studies.mybatis.models.Person;
+import com.hao.studies.mybatis.models.PersonCustom;
+import com.hao.studies.mybatis.models.PersonVo;
 
 public class PersonMapperTest {
 
@@ -17,4 +22,30 @@ public class PersonMapperTest {
 		System.out.println(p);
 	}
 
+	@Test
+	public void testFindByIdResultMap() throws Exception {
+		SqlSession session = SessionFactory.getSession();
+		PersonMapper personMapper = session.getMapper(PersonMapper.class);
+		Person p = personMapper.findByIdResultMap(1);
+		System.out.println(p);
+	}
+
+	@Test
+	public void testFindByList() throws Exception {
+		SqlSession session = SessionFactory.getSession();
+		PersonMapper personMapper = session.getMapper(PersonMapper.class);
+		PersonVo personVo = new PersonVo();
+		PersonCustom pc = new PersonCustom();
+		// pc.setAge(66);
+//		 pc.setName("qq");
+		personVo.setIds(new ArrayList<Integer>() {
+			{
+				add(1);
+				add(6);
+			}
+		});
+		personVo.setPersonCustom(pc);
+		List<Person> p = personMapper.findPersonList(personVo);
+		System.out.println(p);
+	}
 }
