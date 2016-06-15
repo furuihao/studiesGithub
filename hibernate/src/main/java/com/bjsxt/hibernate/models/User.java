bf6afce4719a3660bec4d5796c9f11dcb416d7a0
furuihao <94402146@qq.com>
2016-06-15 17:53:19 +0800

    ManyToOne

---
+++ hibernate/src/main/java/com/bjsxt/hibernate/models/User.java
@@ -0,0 +1,43 @@
+package com.bjsxt.hibernate.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+	private Group group;
+	private int id;
+	private String name;
+
+	@ManyToOne
+	public Group getGroup() {
+		return group;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
