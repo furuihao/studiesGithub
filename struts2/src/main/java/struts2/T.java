0e06d6fb573afa868b72af14b8511d4c394506f1
furuihao <94402146@qq.com>
2016-06-23 01:01:10 +0800

    Struts2 初始化

---
+++ struts2/src/main/java/struts2/T.java
@@ -0,0 +1,14 @@
+package struts2;
+
+import org.apache.commons.logging.Log;
+import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
+
+import freemarker.core.Comment;
+
+public class T {
+	StrutsPrepareAndExecuteFilter f;
+	String s;
+	Log l;
+	Comment c;
+	
+}
