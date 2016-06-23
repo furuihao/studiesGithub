daeff3ae4806d81cff54b7c7bcd90c2d78b33e95
furuihao <94402146@qq.com>
2016-06-23 16:54:43 +0800

    参数传递

--- struts2/src/main/java/struts2/action/UserAction.java
+++ struts2/src/main/java/struts2/action/UserAction.java
@@ -1,18 +1,43 @@
 package struts2.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+import struts2.models.User;
 
-public class UserAction extends ActionSupport {
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+//实现ModeleDriven接口,参数传递使用name=kk&age=19,在Struts2中使用getModel()方法获取User对象,再为对象属性赋值
+//在Action的User要先new();
+public class UserAction extends ActionSupport 
+//implements ModelDriven<User>
+{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -99199186347968583L;
 
+	private User user;
+
 	public String add() {
+		System.out.println(user.getName() + ":" + user.getAge());
 		return "addSuccess";
 	}
+
 	public String delete() {
 		return "deletSuccess";
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+//	@Override
+//	public User getModel() {
+//		return user;
+//	}
+
 }
